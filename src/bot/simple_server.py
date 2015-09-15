25b2970e21c287caea2805f57c7e841fa1cbdf16
Sambhav Satija <sambhav13085@iiitd.ac.in>
2015-09-16 01:20:01 +0530

    Change mappings

--- src/bot/simple_server.py
+++ src/bot/simple_server.py
@@ -3,7 +3,30 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/categories.json')
+def categories():
+    labels_to_data = {}
+
+    with open('dataset.csv', 'r') as f:
+        p = f.read()
+    for collect in p.split("!@#$%^&*()"):
+        # import pdb; pdb.set_trace()
+        collect = collect.strip()
+        collect = collect.split(',')
+        label = collect[0]
+        data = ''.join(collect[1:])
+
+        if label == '' and data == '': continue
+
+        data_collection = labels_to_data.get(label, [])
+        data_collection.append(data)
+        labels_to_data[label] = data_collection
+    return json.dumps(labels_to_data.keys())
+
+
+
+@app.route('/allPosts.json')
 def serve():
     labels_to_data = {}
 
@@ -21,8 +44,7 @@ def serve():
         data_collection = labels_to_data.get(label, [])
         data_collection.append(data)
         labels_to_data[label] = data_collection
-    return json.dumps({
-        "categories": labels_to_data.keys(), "posts": labels_to_data})
+    return json.dumps(labels_to_data)
 
 
 if __name__ == '__main__':
